bab4d9407a78d7c3a76e3a7a7cefc04aba05c6af
Diane Trout <diane@ghic.org>
2016-09-10 11:13:53 -0700

    exclude list needs to be a sequence
    A parenthesized string isn't going to cut it

--- recipe_groups/forms.py
+++ recipe_groups/forms.py
@@ -8,11 +8,11 @@ class CoursePopForm(ModelForm):
     """form object for the popup from the recipe_form to add a new course"""
     class Meta:
         model = Course
-        exclude = ('slug')
+        exclude = ('slug',)
 
 
 class CuisinePopForm(ModelForm):
     """form object for the popup from the recipe_form to add a new cuisine"""
     class Meta:
         model = Cuisine
-        exclude = ('slug')
+        exclude = ('slug',)
