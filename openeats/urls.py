1b23ef025d85d258a638139189d7cc60eccb587b
Diane Trout <diane@debian.ghic.org>
2016-12-31 14:47:53 -0800

    Don't use import * pattern

--- openeats/urls.py
+++ openeats/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import *
 from __future__ import absolute_import
 
+from django.conf.urls import url, include
 from django.conf import settings
 from django.views.static import serve
 from django.views.generic import TemplateView
