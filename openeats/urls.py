46ddff82f705f2a7ed67352c2492a308186e70aa
Diane Trout <diane@debian.ghic.org>
2016-12-31 15:22:26 -0800

    Forgot to import a function for a url view

--- openeats/urls.py
+++ openeats/urls.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+from django.contrib.auth.views import login
 from django.conf.urls import url, include
 from django.conf import settings
 from django.views.static import serve
